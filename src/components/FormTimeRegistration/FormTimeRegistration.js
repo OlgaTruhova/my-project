cd9613531150b0e138b3dd78cde1f416b7d784de
unknown <truhovaolga911@gmail.com>
2021-08-05 23:22:57 +0300

    the design of the time buttons for making an appointment

--- src/components/FormTimeRegistration/FormTimeRegistration.js
+++ src/components/FormTimeRegistration/FormTimeRegistration.js
@@ -7,15 +7,27 @@ export default class FormTimeRegistration extends React.Component {
         timeButton: ['8.00', '10.00', '12.00', '15.00', '17.00']
     }
 
+    getTimeAppointment = (timeButton, appointmentClient, isFree) => {
+        return timeButton.filter(time => {
+            return isFree ?
+            !appointmentClient.find(appointment => appointment.clickTime === time) : null
+        })
+    }
+ 
+
     render () {
-        const {changeHandlerTime} = this.props;
+        const {changeHandlerTime, appointmentClient} = this.props;
         const {timeButton} = this.state;
 
+        const timeNoAppointment = this.getTimeAppointment(timeButton, appointmentClient, true);
+    
         return (
-        
             <div className='wrapper-form-time-registration'>
                 <div className='form-time-registration'>
-                    {timeButton.map(time => <button value={time} onClick={changeHandlerTime}>{time}</button>)}
+                    {timeButton.map(time => 
+                    <button className={!timeNoAppointment.some(time1 => time1 === time) ? 'form-time-registration-disable__button' : 'form-time-registration__button'} 
+                    value={time} key={`key${time}`} disabled={!timeNoAppointment.some(time1 => time1 === time)} 
+                    onClick={changeHandlerTime}>{time}</button>)}
                 </div>
             </div>        
         )
